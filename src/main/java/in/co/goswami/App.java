d89a2203eaae8bdfdce8be4c2699da7377b43c12
Susanta Goswami <susnitdgp@gmail.com>
2022-09-17 14:16:05 +0000

    Fisrt Commit

---
+++ src/main/java/in/co/goswami/App.java
@@ -0,0 +1,13 @@
+package in.co.goswami;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World From Goswami" );
+    }
+}
